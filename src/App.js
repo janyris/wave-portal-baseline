6e183ad9ffc577338688b725939db04075c96740
Janyris <jcocodes@outlook.com>
2021-09-24 21:57:44 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,106 @@
-import React,{ useEffect } from "react";
+import React,{ useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
 
 const App = () => {
-const checkIfWalletIsConnected = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+const checkIfWalletIsConnected = async () => {
+  try {
+  const { ethereum } = window;
 
 
+  if (!ethereum) {
+    console.log("Make sure you have metamask!");
+    return;
+    } else {
+    console.log("We have the ethereum object", ethereum);
+  }
 
-const { ethereum } = window;
+const accounts = await ethereum.request({method: 'eth_accounts' });
 
-
-if (!ethereum) {
-  console.log("Make sure you have metamask!");
-  return;
+if(accounts.length !== 0) {
+  const account = accounts[0];
+  console.log("Found an authorized account:", account);
 } else {
-  console.log("We have the ethereum object", ethereum);
+  console.log("No authorized account found")
+  }
+
+} catch(error){
+  console.log(error);
+
+  }
+
+}
+
+
+const connectWallet = async () => {
+  try {
+    const { ethereum } = window;
+
+    if (!ethereum){
+      alert("Get Metamask!");
+      return;
+    }
+
+const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+console.log("Connected", accounts[0]);
+setCurrentAccount(accounts[0]);
+  } catch (error) {
+    console.log(error)
+
+
   }
 }
+
+useEffect(() =>{
+  checkIfWalletIsConnected();
+}, [])
+
+
+
+
+
+// const checkIfWalletIsConnected = () => {
+
+
+
+// const { ethereum } = window;
+
+
+// if (!ethereum) {
+//   console.log("Make sure you have metamask!");
+//   return;
+// } else {
+//   console.log("We have the ethereum object", ethereum);
+//   }
+// }
 
   return (
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">🌈 What's Up!</div>
+        <div className="intro"> I'm Janyris.</div>
 
-        <div className="bio">I'm Janyris. I'm a Designer and Developer learning Blockchain Development. Connect your Ethereum wallet and wave at me!</div>
+        <div className="bio"> I'm a Designer and Developer learning Blockchain Development. Connect your Ethereum wallet and wave at me!</div>
 
         <button className="waveButton" onClick={null}>
           👋🏽 Wave at Me
         </button>
+{/*
+*If there is no currentAccount render this button
+*/}
+{!currentAccount && (
+<button className="waveButton" onClick={null}>
+🦊 Connect Wallet
+</button>
+
+)}
+
+
+
+
       </div>
     </div>
   );
